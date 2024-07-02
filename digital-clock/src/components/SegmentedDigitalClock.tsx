fbf410f381bb525708ce8de6ab57668b3748d0c3
nishat65 <nishatroy5@gmail.com>
2024-07-02 19:02:45 +0530

    added digital clock and segemented digi clock

---
+++ digital-clock/src/components/SegmentedDigitalClock.tsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+import "./DigitalClock.css";
+
+const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const getDigits = (number: number) => {
+    return number.toString().padStart(2, "0").split("");
+  };
+
+  const renderDigit = (digit: string) => {
+    const segments = [
+      digit !== "1" && digit !== "4",
+      digit !== "5" && digit !== "6",
+      digit !== "2",
+      digit !== "1" && digit !== "4" && digit !== "7",
+      digit === "0" || digit === "2" || digit === "6" || digit === "8",
+      digit !== "1" && digit !== "2" && digit !== "3" && digit !== "7",
+      digit !== "0" && digit !== "1" && digit !== "7",
+    ];
+    console.log(segments);
+    return (
+      <div className="digit">
+        {segments.map((active, index) => (
+          <div key={index} className={`segment ${active ? "active" : ""}`} />
+        ))}
+      </div>
+    );
+  };
+
+  const hours = getDigits(time.getHours());
+  const minutes = getDigits(time.getMinutes());
+  const seconds = getDigits(time.getSeconds());
+
+  return (
+    <div className="digital-clock">
+      {renderDigit(hours[0])}
+      {renderDigit(hours[1])}
+      <div className="colon">:</div>
+      {renderDigit(minutes[0])}
+      {renderDigit(minutes[1])}
+      <div className="colon">:</div>
+      {renderDigit(seconds[0])}
+      {renderDigit(seconds[1])}
+    </div>
+  );
+};
+
+export default DigitalClock;
